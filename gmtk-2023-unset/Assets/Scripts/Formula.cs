37e151251cb7d8c46989dcb3d4a53771c0029c56
Mirroray41 <87991171+Mirroray41@users.noreply.github.com>
2023-07-09 13:26:42 +0200

    added delay to formula take off

--- gmtk-2023-unset/Assets/Scripts/Formula.cs
+++ gmtk-2023-unset/Assets/Scripts/Formula.cs
@@ -3,33 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class Formula : MonoBehaviour
 {
-	public float formulaSpeed;
-	public float startX;
-	private float stopX;
-	public float partsNeaded;
-	private float partsRecived;
-	public float endX;
-	public string nextScene;
+    public float formulaSpeed;
+    public float startX;
+    private float stopX;
+    public float partsNeaded;
+    private float partsRecived;
+    public float endX;
+    public float delay = 2f;
+    public string nextScene;
+    float timer;
+    void Awake()
+    {
+        stopX = transform.position.x;
+        transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+    }
 
-	void Awake()
-	{
-		stopX = transform.position.x;
-		transform.position = new Vector3(startX, transform.position.y, transform.position.z);
-	}
-	void FixedUpdate()
-	{
-		if ((partsRecived < partsNeaded && transform.position.x > stopX) || partsRecived >= partsNeaded)
-		{
-			transform.position -= new Vector3(formulaSpeed * Time.deltaTime, 0f, 0f);
-			if (transform.position.x < endX) SceneManager.LoadScene(nextScene);
-		}
-	}
+    void FixedUpdate()
+    {
+        if (partsRecived < partsNeaded && transform.position.x > stopX)
+        {
+            transform.position -= new Vector3(formulaSpeed * Time.deltaTime, 0f, 0f);
+        }
+        else if (partsRecived >= partsNeaded)
+        {
+            timer += Time.deltaTime;
+            if (timer > delay)
+            {
+                transform.position -= new Vector3(formulaSpeed * Time.deltaTime, 0f, 0f);
+            }
+
+            if (transform.position.x < endX) SceneManager.LoadScene(nextScene);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Part")
         {
-	        partsRecived++;
-	        Destroy(collision.gameObject);
+            partsRecived++;
+            Destroy(collision.gameObject);
         }
     }   
 }
