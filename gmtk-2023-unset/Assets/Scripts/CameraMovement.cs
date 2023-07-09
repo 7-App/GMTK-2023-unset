c9cc109e5d75e4f7b46a923be7e1017eeebfdcd0
Mirroray41 <87991171+Mirroray41@users.noreply.github.com>
2023-07-09 15:32:22 +0200

    redo of tilemaps and better colisions

--- gmtk-2023-unset/Assets/Scripts/CameraMovement.cs
+++ gmtk-2023-unset/Assets/Scripts/CameraMovement.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public GameObject playerBody;
+    public float MaxX;
+    public float MinX;
+    public float MaxY;
+    public float MinY;
+    
     void FixedUpdate()
     {
-        if (playerBody.transform.position.x > 10 || playerBody.transform.position.x < -10)
+        if (playerBody.transform.position.x >= MaxX || playerBody.transform.position.x <= MinX)
         {
-            if (playerBody.transform.position.y > 10 || playerBody.transform.position.y < -10) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            if (playerBody.transform.position.y >= MaxY || playerBody.transform.position.y <= MinY) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             else                                                                               transform.position = new Vector3(transform.position.x, playerBody.transform.position.y, transform.position.z);
         }
-        else if (playerBody.transform.position.y > 10 || playerBody.transform.position.y < -10)
+        else if (playerBody.transform.position.y >= MaxY || playerBody.transform.position.y <= MinY)
         {
-            if (playerBody.transform.position.z > 10 || playerBody.transform.position.z < -10) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            if (playerBody.transform.position.x >= MaxX || playerBody.transform.position.x <= MinX) transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             else                                                                               transform.position = new Vector3(playerBody.transform.position.x, transform.position.y, transform.position.z);
         }
         else transform.position = playerBody.transform.position + new Vector3(0,0,transform.position.z);
