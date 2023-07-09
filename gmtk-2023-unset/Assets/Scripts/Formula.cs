eb2e4a7158b8920f91eafbae63233b88aa938ab8
KrystofCL <Krystof.Hava@hotmail.com>
2023-07-09 12:04:36 +0200

    ¯\_(ツ)_/¯

--- gmtk-2023-unset/Assets/Scripts/Formula.cs
+++ gmtk-2023-unset/Assets/Scripts/Formula.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class Formula : MonoBehaviour
 {
+	public float formulaSpeed;
+	public float startX;
+	private float stopX;
+	public float partsNeaded;
+	private float partsRecived;
+	public float endX;
+
+	void Awake()
+	{
+		stopX = transform.position.x;
+		transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+	}
+	void FixedUpdate()
+	{
+		if ((partsRecived < partsNeaded && transform.position.x > stopX) || partsRecived >= partsNeaded)
+		{
+			transform.position -= new Vector3(formulaSpeed * Time.deltaTime, 0f, 0f);
+			//if(transform.position.x < endX) TODO: End Level
+		}
+	}
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Part")
         {
-	        if(collision.gameObject.GetComponent<Part>().isCorrect)
-			{
-				Debug.Log("Ya");	
-			}
-            else
-            {
-	            Debug.Log("Nein");
-            }
+	        partsRecived++;
 	        Destroy(collision.gameObject);
         }
     }   
